f22648ee9a0e3065abe4efc6882c3b5714cf312f
Dayveed12345 <113670108+Dayveed12345@users.noreply.github.com>
2023-10-01 05:10:46 +0100

    Added the aws configuratins

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Carbon;
 class VideoController extends Controller
 {
 
-
+    public function storePath($disk, $path)
+    {
+        return Storage::disk($disk)->path($path);
+    }
     // ...
 
     public function uploadVideo(Request $request)
@@ -22,63 +25,67 @@ class VideoController extends Controller
             'video' => 'required|file|mimetypes:video/*|max:204800',
         ]);
 
-        // Store the video file on the "videos" disk
         $uploadedVideo = $request->file('video');
-        // Get the name of the uploaded video file
-        $videoName = $uploadedVideo->getClientOriginalName();
-        $path = $uploadedVideo->storeAs('public',$videoName);
-        // dd($path);
-        if($path):
-        // Get the size of the uploaded video file in bytes
-        $videoInByte = $uploadedVideo->getSize() /(1024 * 1024);
-        //Converting to KiloByte;
-        $videoSize=round($videoInByte,2)."mb";
 
-        // Get the video length using getID3
-        $getID3 = new getID3();
-        $videoFilePath = Storage::disk('public')->path($videoName);
-      // Get the full path to the video file
-        $fileInfo = $getID3->analyze($videoFilePath);
-        // Checking if the User
-        $videoLength = isset( $fileInfo['playtime_string']) ?$fileInfo['playtime_string']: null;
-        // Store The video and other credentials to the database
-        $video = new Video;
-        $video->name = $videoName;
-        $video->size = $videoSize;
-        $video->length = $videoLength;
-        $video->path = $videoFilePath;
-        $video->uploaded_time=Carbon::now();
-        $checking = $video->save();
-        if ($checking) :
-            return response()->json([
-                'StatusCode' => 201,
-                'message' => 'Image has been uploaded successfully',
-                'status' => 'success',
-                'data' => [
-                    'video_name' => $videoName,
-                    'video_size' => $videoSize,
-                    'video_length' => $videoLength,
-                    'video_path' => $videoFilePath
-                ]
-            ]);
+        $videoName = $uploadedVideo->getClientOriginalName();
+
+        $path = $uploadedVideo->storeAs('videos', $videoName);
+        $localPath = $uploadedVideo->storeAs('videos', $videoName, 'public');
+
+        if ($path) :
+
+            $videoInByte = $uploadedVideo->getSize() / (1024 * 1024);
+            $videoSize = round($videoInByte, 2) . "mb";
+            $getID3 = new getID3();
+            $videoFilePath = $this->storePath('s3', $path);
+            $localVideoFilePath = $this->storePath('local', 'public/' . $localPath);
+            $fullVideoPath = 'https://hng-video-upload.s3.us-east-1.amazonaws.com/' . $videoFilePath;
+            $fileInfo = $getID3->analyze($localVideoFilePath);
+            $videoLength = isset($fileInfo['playtime_string']) ? $fileInfo['playtime_string'] : '00.00';
+            $video = new Video;
+            $video->name = $videoName;
+            $video->size = $videoSize;
+            $video->length = $videoLength;
+            $video->path = $fullVideoPath;
+            $video->uploaded_time = Carbon::now();
+            $checking = $video->save();
+            // Deleting the file from local storage
+            $filePathToDelete = 'app/public/videos/' . $videoName;
+            $fullFilePath = storage_path($filePathToDelete);
+            unlink($fullFilePath);
+            if ($checking) :
+                return response()->json([
+                    'StatusCode' => 201,
+                    'message' => 'Image has been uploaded successfully',
+                    'status' => 'success',
+                    'data' => [
+                        'video_name' => $videoName,
+                        'video_size' => $videoSize,
+                        'video_length' => $videoLength,
+                        'video_path' => $fullVideoPath
+                    ]
+                ]);
+            else :
+                return response()->json([
+                    'StatusCode' => 400,
+                    'status' => 'error',
+                    'message' => 'Bad Request an Error Occurred',
+
+                ]);
+            endif;
         else :
             return response()->json([
-                'StatusCode' => 400,
-                'message' => 'Bad Request an Error Occurred',
+                'StatusCode' => 401,
                 'status' => 'error',
+                'message' => 'An Error occurred While trying to Save file ',
+
             ]);
         endif;
-    else:
-        return response()->json([
-            'StatusCode' => 401,
-            'message' => 'An Error occurred While trying to Save file ',
-            'status' => 'error',
-        ]);
-    endif;
     }
-    public function getVideo(){
-        $video=Video::all(['name','size','length','path','uploaded_time']);
-        if ($video) :
+    public function getVideo()
+    {
+        $video = Video::all(['name', 'size', 'length', 'path', 'uploaded_time']);
+        if (!$video->isEmpty()) :
             return response()->json([
                 'StatusCode' => 200,
                 'message' => 'Image displayed Successfully',
@@ -87,11 +94,10 @@ class VideoController extends Controller
             ]);
         else :
             return response()->json([
-                'StatusCode' => 400,
-                'message' => 'An Error Occurred',
+                'StatusCode' => 404,
+                'message' => 'No data found',
                 'status' => 'error',
-            ]);
+            ], 404);
         endif;
-
     }
 }
