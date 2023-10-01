a2026fcd1edd1a715850bf7f9491e0037470bde7
Dayveed12345 <113670108+Dayveed12345@users.noreply.github.com>
2023-10-01 18:43:45 +0100

    Added the file

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -31,7 +31,7 @@ class VideoController extends Controller
         ]);
 
         $uploadedVideo = $request->file('video');
-        $videoName=$request->all(['original_name']);
+        $videoName=$request->all();
 
         $path = $uploadedVideo->storeAs('videos',   $videoName);
         $localPath = $uploadedVideo->storeAs('videos',   $videoName, 'public');
